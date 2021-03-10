d46b28368869328fb259444ea2b8e55f21ba186f
Mullatoez <kimanthikevin76@gmail.com>
2021-03-10 14:58:27 +0300

    Youtube clone

--- app/src/main/java/com/example/pone/MainAdapter.kt
+++ app/src/main/java/com/example/pone/MainAdapter.kt
@@ -3,8 +3,10 @@ package com.example.pone
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<MainAdapter.CustomViewHolder>() {
 
@@ -26,6 +28,16 @@ class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<MainAdapter.Cus
         val video = homeFeed.videos[position]
         holder.video_title.text = video.name
         holder.channel_name.text = video.channel.name
+
+        val thumbnailImageView = holder.thumb_nail
+        Picasso.with(holder.itemView.context).load(video.imageUrl).into(
+            thumbnailImageView
+        )
+
+        val channelImageView = holder.channel_image
+        Picasso.with(holder.itemView.context).load(video.channel.profileImageUrl).into(
+            channelImageView
+        )
     }
 
 
@@ -34,6 +46,10 @@ class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<MainAdapter.Cus
         val video_title: TextView = itemView.findViewById(R.id.video_title)
 
         val channel_name: TextView = itemView.findViewById(R.id.channel_name)
+
+        val thumb_nail: ImageView = itemView.findViewById(R.id.video_thumbnail)
+
+        val channel_image: ImageView = itemView.findViewById(R.id.channel_image)
     }
 
 }
